f05930e9adb3d162fd151719ccd1da4e8817210f
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-07-13 22:45:42 +0900

    各頂点毎に判定するよう修正

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,5 @@
 #include <SFML/Graphics.hpp>
+#include <utility>
 #include <vector>
 #include "Vec2.h"
 
@@ -33,8 +34,8 @@ int main()
                             sf::Style::Titlebar | sf::Style::None);
 
     std::vector<sf::Shape*> shapes;
-
-    sf::Vector2f vec;
+    std::vector<Vec2> points;
+    std::vector<std::pair<Vec2, Vec2>> lines;
 
     /*sf::CircleShape triangle(50.0f, 3);
     triangle.setFillColor(sf::Color::Blue);
@@ -45,6 +46,21 @@ int main()
     rect.setFillColor(sf::Color::Blue);
     rect.setPosition(sf::Vector2f(600.0f, 100.0f));
     shapes.push_back(&rect);
+
+    for (int i = 0; i < rect.getPointCount(); i++)
+    {
+        auto& point = rect.getTransform().transformPoint(rect.getPoint(i));
+        points.push_back(Vec2(point.x, point.y));
+    }
+
+    for (int i = 1; i <= rect.getPointCount(); i++)
+    {
+        auto& pointA = rect.getTransform().transformPoint(rect.getPoint(i - 1));
+        Vec2 a       = Vec2(pointA.x, pointA.y);
+        auto& pointB = rect.getTransform().transformPoint(rect.getPoint(i % rect.getPointCount()));
+        Vec2 b       = Vec2(pointB.x, pointB.y);
+        lines.push_back(std::make_pair(a, b));
+    }
 
     /*sf::ConvexShape convex;
     convex.setPointCount(5);
@@ -91,29 +107,39 @@ int main()
 
         window.draw(rect);
 
-        Vec2 a(rect.getTransform().transformPoint(rect.getPoint(2)).x,
-               rect.getTransform().transformPoint(rect.getPoint(2)).y);
-        Vec2 b(rect.getTransform().transformPoint(rect.getPoint(3)).x,
-               rect.getTransform().transformPoint(rect.getPoint(3)).y);
-
         auto& mousePosition = sf::Mouse::getPosition(window);
+        Vec2 mousePos       = Vec2(mousePosition.x, mousePosition.y);
 
-        Vec2 c(mousePosition.x, mousePosition.y);
-        Vec2 d(mousePosition.x, 0.0);
-
-        auto& intersect = lineIntersect(a, b, c, d);
-        if (intersect.result)
+        for (auto& point : points)
         {
-            Vec2& position = intersect.position;
-            intersectPoint.setPosition(sf::Vector2f(position.x, position.y));
+            Intersect first = {false, Vec2(0, 0)};
+            for (auto& l : lines)
+            {
+                auto& intersect = lineIntersect(l.first, l.second, mousePos, point);
+                if (intersect.result)
+                {
+                    if (first.result)
+                    {
+                        Vec2 intersectToMouse = mousePos - intersect.position;
+                        Vec2 firstToMouse     = mousePos - first.position;
+                        if (intersectToMouse.length() < firstToMouse.length())
+                        {
+                            first = intersect;
+                        }
+                    }
+                    else
+                    {
+                        first = intersect;
+                    }
+                }
+            }
 
-            Vec2 fromCenter = position - Vec2(mousePosition.x, mousePosition.y);
-
-            line.setSize(sf::Vector2f(fromCenter.length(), 5.0f));
-            line.setPosition(sf::Vector2f(mousePosition.x, mousePosition.y));
-
-            window.draw(line);
-            window.draw(intersectPoint);
+            if (first.result)
+            {
+                Vec2& position = first.position;
+                intersectPoint.setPosition(sf::Vector2f(position.x, position.y));
+                window.draw(intersectPoint);
+            }
         }
 
         window.display();
