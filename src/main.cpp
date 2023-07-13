27039f5faa097b2f8efcd49635e0a805c8c41009
kdr250 <54400975+kdr250@users.noreply.github.com>
2023-07-13 19:50:25 +0900

    いったん正方形1個だけで考える

--- src/main.cpp
+++ src/main.cpp
@@ -9,17 +9,17 @@ int main()
 
     std::vector<sf::Shape*> shapes;
 
-    sf::CircleShape triangle(50.0f, 3);
+    /*sf::CircleShape triangle(50.0f, 3);
     triangle.setFillColor(sf::Color::Blue);
     triangle.setPosition(sf::Vector2f(100.0f, 100.0f));
-    shapes.push_back(&triangle);
+    shapes.push_back(&triangle);*/
 
     sf::RectangleShape rect(sf::Vector2f(100.0f, 100.0f));
     rect.setFillColor(sf::Color::Blue);
     rect.setPosition(sf::Vector2f(600.0f, 100.0f));
     shapes.push_back(&rect);
 
-    sf::ConvexShape convex;
+    /*sf::ConvexShape convex;
     convex.setPointCount(5);
     convex.setPoint(0, sf::Vector2f(300.0f, 600.0f));
     convex.setPoint(1, sf::Vector2f(450.0f, 610.0f));
@@ -27,7 +27,12 @@ int main()
     convex.setPoint(3, sf::Vector2f(330.0f, 700.0f));
     convex.setPoint(4, sf::Vector2f(300.0f, 650.0f));
     convex.setFillColor(sf::Color::Blue);
-    shapes.push_back(&convex);
+    shapes.push_back(&convex);*/
+
+    sf::RectangleShape line(sf::Vector2f(window.getSize().y / 2.0f, 5.0f));
+    line.setPosition(window.getSize().x / 2, window.getSize().y / 2);
+    line.rotate(270.0f);
+    line.setFillColor(sf::Color::Yellow);
 
     while (window.isOpen())
     {
@@ -49,10 +54,13 @@ int main()
 
         window.clear(sf::Color::Black);
 
-        for (auto& shape : shapes)
+        /*for (auto& shape : shapes)
         {
             window.draw(*shape);
-        }
+        }*/
+
+        window.draw(rect);
+        window.draw(line);
 
         window.display();
     }
